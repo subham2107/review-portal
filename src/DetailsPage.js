0cb8361083c54ca16e738d131f885af6708d6675
Subham Mitra <subham.mitra21@gmail.com>
2021-06-01 00:06:17 +0530

    cast image alignment fixed

--- src/DetailsPage.js
+++ src/DetailsPage.js
@@ -181,7 +181,7 @@ reviewClick = (e) => {
           <div className="detailsPageCasts" >
         {(this.state.cast).map((eachCast) => (
         <div className="eachCastDiv">
-          <div>
+          <div class='cast-card'>
           <img class='profile-poster' src={`https://image.tmdb.org/t/p/w500${eachCast.profile_path}`}/>
           <p className='cast-name'>{(eachCast.name)}</p>
           </div>
@@ -193,8 +193,6 @@ reviewClick = (e) => {
           <h3>Reviews & Ratings: </h3>
           {(this.state.movie.reviews).slice(0).reverse().map((eachReview) => (
           <div className='review-rating' >
-          {/* {(eachReview.review)||(eachReview.rating)?<li>Review: {(eachReview.review)} </li>:null}
-          {(eachReview.review)||(eachReview.rating)?<span> Rating: {(eachReview.rating)}</span>:null} */}
           <li>{(eachReview.rating)} <svg width="13" height="13" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="yellow" ><path d="M12 20.1l5.82 3.682c1.066.675 2.37-.322 2.09-1.584l-1.543-6.926 5.146-4.667c.94-.85.435-2.465-.799-2.567l-6.773-.602L13.29.89a1.38 1.38 0 0 0-2.581 0l-2.65 6.53-6.774.602C.052 8.126-.453 9.74.486 10.59l5.147 4.666-1.542 6.926c-.28 1.262 1.023 2.26 2.09 1.585L12 20.099z"></path></svg> , {(eachReview.review)}  </li>
           
           </div>
